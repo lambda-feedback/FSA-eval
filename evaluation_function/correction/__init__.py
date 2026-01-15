868c14ba413e4ea7cab09d0e97c06f17f9741d32
zr523 <zr523@ic.ac.uk>
2026-01-15 10:27:46 +0000

    feat: added init for correction

---
+++ evaluation_function/correction/__init__.py
@@ -0,0 +1,55 @@
+"""
+FSA Correction Module
+
+Provides diagnostic information for comparing student FSAs against expected languages:
+- Generate strings that show differences between student FSA and expected language
+- Identify specific states/transitions causing errors
+
+Main Functions:
+- analyze_fsa_correction: Full pipeline for FSA comparison and error identification
+- get_correction_feedback: Convenience function returning dict results
+- check_fsa_properties: Get structural properties of a single FSA
+- quick_equivalence_check: Fast check if two FSAs are equivalent
+"""
+
+from .correction import (
+    # Main pipeline functions
+    analyze_fsa_correction,
+    get_correction_feedback,
+    check_fsa_properties,
+    quick_equivalence_check,
+    
+    # Helper functions
+    trace_string,
+    fsa_accepts,
+    generate_difference_strings,
+    identify_state_errors,
+    identify_transition_errors,
+    
+    # Result classes
+    CorrectionResult,
+    DifferenceString,
+    TransitionError,
+    StateError,
+)
+
+__all__ = [
+    # Main pipeline
+    "analyze_fsa_correction",
+    "get_correction_feedback",
+    "check_fsa_properties",
+    "quick_equivalence_check",
+    
+    # Helpers
+    "trace_string",
+    "fsa_accepts",
+    "generate_difference_strings",
+    "identify_state_errors",
+    "identify_transition_errors",
+    
+    # Classes
+    "CorrectionResult",
+    "DifferenceString",
+    "TransitionError",
+    "StateError",
+]
